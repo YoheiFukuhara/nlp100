d62e0dfd893a7e6a85b8a2c517cf2074c6c3ce6d
YoheiFukuhara <yohei.fukuhara@gmail.com>
2020-02-24 11:57:56 +0900

    2020/02/24

--- 05.係り受け解析/045_shell.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sort 045_result.txt | uniq --count | sort --numeric-sort --reverse > "045_all.txt"
-grep "^する\s" 045_result.txt | sort | uniq --count | sort --numeric-sort --reverse > "045_する.txt"
-grep "^見る\s" 045_result.txt | sort | uniq --count | sort --numeric-sort --reverse > "045_見る.txt"
-grep "^与える\s" 045_result.txt | sort | uniq --count | sort --numeric-sort --reverse > "045_与える.txt"
